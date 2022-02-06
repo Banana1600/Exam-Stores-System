4e8c2af5994a03d9748235b1032d18c5da837f5f
suprakron <suprakron15@gmail.com>
2022-02-07 02:53:28 +0700

    start server result

--- examproject/exam/views.py
+++ examproject/exam/views.py
@@ -2,6 +2,10 @@ from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+
+
+
+
 # Create your views here.
 def index(request):
-    return render(request,'templates\frontend\index.html')
+    return render(request,'index.html')
