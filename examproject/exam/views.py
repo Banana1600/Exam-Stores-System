5c9d5387a26852cefb8fd9ab28bf2147279a252f
Banana1600 <61030031@kmitl.ac.th>
2022-02-09 23:08:34 +0700

    create login new and create choice

--- examproject/exam/views.py
+++ examproject/exam/views.py
@@ -9,3 +9,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def choice(request):
+    return render(request,'choice.html')
--- examproject/static/as/script/login.js
+++ examproject/static/as/script/login.js
@@ -1,17 +1,12 @@
-        const signInBtn = document.getElementById("signIn");
-        const signUpBtn = document.getElementById("signUp");
-        const fistForm = document.getElementById("form1");
-        const secondForm = document.getElementById("form2");
-        const container = document.querySelector(".container");
-        
-        signInBtn.addEventListener("click", () => {
-            container.classList.remove("right-panel-active");
-        });
-        
-        signUpBtn.addEventListener("click", () => {
-            container.classList.add("right-panel-active");
-        });
-        
-        fistForm.addEventListener("submit", (e) => e.preventDefault());
-        secondForm.addEventListener("submit", (e) => e.preventDefault());
-    
+var left = document.querySelector('.left');
+var right = document.querySelector('.right');
+var signUp = document.querySelector('.signUpBtn');
+var signIn = document.querySelector('.signInBtn');
+signUp.addEventListener('click', function() {
+    left.classList.add('flip');
+    right.classList.add('flip');
+});
+signIn.addEventListener('click', function() {
+    left.classList.remove('flip');
+    right.classList.remove('flip');
+});
